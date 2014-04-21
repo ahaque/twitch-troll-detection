150ce9e314fee4b1d1dc287e715b16a4cde6f08c
Albert Haque <ahaque@cs.utexas.edu>
2014-04-21 16:19:28 -0500

    Finished MessageGrouper

--- mapreduce/src/MessageGrouper.java
+++ mapreduce/src/MessageGrouper.java
@@ -8,7 +8,6 @@
  */
 
 import java.io.IOException;
-import java.util.*;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
@@ -21,34 +20,33 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class MessageGrouper {
 
-	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-		private final static IntWritable one = new IntWritable(1);
-		private Text word = new Text();
-
+	public static class Map extends Mapper<LongWritable, Text, Text, Text> {
+		
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 			String line = value.toString();
-			StringTokenizer tokenizer = new StringTokenizer(line);
-			while (tokenizer.hasMoreTokens()) {
-				word.set(tokenizer.nextToken());
-				context.write(word, one);
-			}
+			// Extract the username
+			int usernameStartIndex = line.indexOf("<user>")+6;
+			int usernameEndIndex = line.indexOf("</user>");
+			// Key is username, message is value
+			context.write(new Text(line.substring(usernameStartIndex, usernameEndIndex)), new Text(value.toString()));
 		}
+		
 	}
 
-	public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+	public static class Reduce extends Reducer<Text, Text, Text, Text> {
 
-		public void reduce(Text key, Iterable<IntWritable> values,
-				Context context) throws IOException, InterruptedException {
-			int sum = 0;
-			for (IntWritable val : values) {
-				sum += val.get();
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+			StringBuilder builder = new StringBuilder();
+			for (Text t : values) {
+				builder.append(t+"\n");
 			}
-			context.write(key, new IntWritable(sum));
+			context.write(key, new Text(builder.toString()));
 		}
 	}
 
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
+		@SuppressWarnings("deprecation")
 		Job job = new Job(conf, "MessageGrouper");
 
 		job.setOutputKeyClass(Text.class);
