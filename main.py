070f855ceed9019c9ea9e5a8766b69c9a9593c52
Albert Haque <ahaque@cs.utexas.edu>
2014-04-23 18:17:55 -0500

    Added main file

---
+++ main.py
@@ -0,0 +1,31 @@
+# Author: Albert Haque
+# Date: April 2014
+# Twitch Plays Pokemon, Machine Learns Twitch
+
+import sys, os
+
+class Context:
+    def __init__(self):
+        i = 0
+
+def main():
+    # Input error handling
+    usage_message = "   USAGE: python main.py <dataset file>"  
+    if len(sys.argv) != 2:
+        print("   ERROR: You must supply all program arguments. You entered: " + str(len(sys.argv)-1) + " arguments.")
+        print(usage_message)
+        sys.exit(0)
+    if os.path.isfile(sys.argv[1]) is False:
+        print("   ERROR: The input file \"" + sys.argv[1] + "\" does not exist.")
+        print(usage_message)
+        sys.exit(0)
+
+    input_file = open(sys.argv[1], "r")
+    lines = input_file.readlines()
+    count = 0
+    for line in lines:
+        count += 1
+    print count
+
+if  __name__ =='__main__':
+    main()
