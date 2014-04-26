068cf85ea85c20de60b30eee8f3dc436fe83caa4
Albert Haque <ahaque@cs.utexas.edu>
2014-04-26 01:34:18 -0500

    Added month and day padding

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 # Date: April 2014
 # Twitch Plays Pokemon, Machine Learns Twitch
 
-import sys, os
+import sys
+import os
 
 class Context:
     def __init__(self):
@@ -21,11 +22,45 @@ def main():
         sys.exit(0)
 
     input_file = open(sys.argv[1], "r")
+    date_frequency = dict()
+
     lines = input_file.readlines()
     count = 0
     for line in lines:
+        date_raw = line[line.find("<date>")+6:line.find("</date>")]
+        # Convert 1 digit numbers to 2 digits
+        year = date_raw[0:4]
+        month = padMonth(date_raw)
+        day = padDay(date_raw)
+        formatted_date = year + "-" + month + "-" + day
+        
+        if formatted_date in date_frequency:
+            date_frequency[formatted_date] += 1
+        else:
+            date_frequency[formatted_date] = 1
         count += 1
-    print count
+    
+    for key in date_frequency.keys():
+        print key + "\t" + str(date_frequency[key])
+    print "Total Messages: " + str(count)
+
+def padMonth(date_raw):
+    first_dash = 5
+    second_dash = date_raw[first_dash:].find("-") + first_dash
+    month = date_raw[first_dash:second_dash]
+    if len(month) == 1:
+        return "0" + month
+    else:
+        return month
+
+def padDay(date_raw):
+    first_dash = 5
+    second_dash = date_raw[first_dash:].find("-") + first_dash
+    day = date_raw[second_dash+1:]
+    if len(day) == 1:
+        return "0" + day
+    else:
+        return day
 
 if  __name__ =='__main__':
     main()
